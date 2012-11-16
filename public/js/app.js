4733a95baf7e1673af17b977f7d54dbb28e2ae20
xthexder <xthexder@live.com>
2012-11-16 00:40:45 -0500

    Added google analytics

--- public/js/app.js
+++ public/js/app.js
@@ -103,7 +103,18 @@ function makeRequest(type, url, reqdata, callback) {
   });
 }
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-20354481-13']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(function() {
+
   var $window = $(window)
     , addedScroll = false;
 
